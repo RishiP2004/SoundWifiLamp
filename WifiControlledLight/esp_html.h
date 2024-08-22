2df00113f368e9d310ed5fc000460ef241b24100
DavidyKwok <156530176+DavidyKwok@users.noreply.github.com>
2024-08-22 14:56:02 -0400

    Update esp_html.h

--- WifiControlledLight/esp_html.h
+++ WifiControlledLight/esp_html.h
@@ -26,9 +26,14 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
             font-weight: bold;
         }
         .container {
-            max-width: 800px;
+            max-width: 600px;
+            height:300px;
             margin: 20px auto;
             text-align: center;
+            background-color: lightgrey;
+            border-radius:10px;
+            border: 2px solid #333;
+            box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.3);
         }
         .btn {
             background-color: #444444;
@@ -48,19 +53,82 @@ const char PAGE_MAIN[] PROGMEM = R"=====(
             color: #AAAAAA;
             padding: 10px 0;
         }
+        /*button*/
+        h1 {
+            color: green;
+        }
+        .toggle {
+            position : relative ;
+            display : inline-block;
+            width : 100px;
+            height : 52px;
+            background-color: red;
+            border-radius: 30px;
+            border: 2px solid gray;
+        }
+              
+        .toggle:after {
+            content: '';
+            position: absolute;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            background-color: gray;
+            top: 1px; 
+            left: 1px;
+            transition:  all 0.5s;
+        }
+              
+        p {
+            font-family: Verdana, Arial, sans-serif;
+            font-weight: bold;
+            color: white;
+        }
+              
+        .checkbox:checked + .toggle::after {
+            left : 49px; 
+        }
+              
+        /* Checkbox checked toggle label bg color */
+        .checkbox:checked + .toggle {
+            background-color: green;
+        }
+              
+        /* Checkbox vanished */
+        .checkbox { 
+            display : none;
+        }
+        .toggleButton{
+            padding-top:40px;
+            padding-bottom:40px;
+        }
     </style>
 </head>
 <body>
     <div class="navbar">ESP32 Control</div>
     <div class="container">
+        <br>
         <h2>Control LED</h2>
-        <button type="button" class="btn" id="btnOn" onclick="ButtonPress('ON')">Turn LED ON</button>
-        <button type="button" class="btn" id="btnOff" onclick="ButtonPress('OFF')">Turn LED OFF</button>
-    </div>
-    <footer class="foot">Lamp Control Page</footer>
+        <div class="toggleButton">
+            <center>
+            <input type="checkbox" id="switch" class="checkbox" onclick="sendToggleState()" />
+            <label for="switch" class="toggle">
+                <p>
+                    ON &nbsp;&nbsp; OFF
+                </p>
+            </label>
+            </center>
+        </div>
+
+    
+    <footer class="foot">ESP32 Web Page</footer>
 
     <script type="text/javascript">
-        function ButtonPress(state) {
+        function sendToggleState() {
+            var switchElement = document.getElementById('switch');
+            var state = switchElement.checked ? 'ON' : 'OFF';
+            console.log('Current toggle state:', state);
+
             var xhttp = new XMLHttpRequest();
             xhttp.open("PUT", "/BUTTON_" + state, true);
             xhttp.send();
